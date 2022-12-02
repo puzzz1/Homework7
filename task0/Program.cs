2e367b5104985111bafbcecc8b769eb8068e1263
puzz <taste@inbox.ru>
2022-12-02 22:56:21 +0300

    Make homework again

---
+++ task0/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+double[,] generate2DArray(int lengthRow, int lengthCol, int deviation)
+{
+    double[,] array = new double[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+
+    }
+    return array;
+}
+void printArray(double[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+double[,] array = generate2DArray(3, 4, 10);
+printArray(array);
---
+++ task2/Program.cs
@@ -0,0 +1,55 @@
+﻿/* Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+int[,] generate2DArray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow, lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i, j] = new Random().Next(- deviation, deviation + 1);
+        }
+
+    }
+    return array;
+}
+void printArray(int[,] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+        Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+void ResultArray (int [,] array)
+{
+    double midlmean = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        double result = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result = result + array[i,j];
+        }
+        midlmean = result / array.GetLength(0);
+        Console.WriteLine($" Среднеарифмметическое столбца {i} - {result}");
+    }
+    {
+        
+    }
+}
+
+int[,] array = generate2DArray(3, 4, 10);
+printArray(array);
+Console.WriteLine();
+ResultArray(array);
